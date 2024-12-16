0c7b35f7767e76a07766448b0dea63c0e9d8d46e
Eaelllfaust <eaelllfaust@gmail.com>
2024-12-16 22:19:24 +0100

    changes

--- client/src/pages/student/home/index.jsx
+++ client/src/pages/student/home/index.jsx
@@ -1,5 +1,5 @@
 import { courseCategories } from "@/config";
-import banner from "../../../../public/banner-img.png";
+import banner from "../../../../public/banner.jpg";
 import { Button } from "@/components/ui/button";
 import { useContext, useEffect } from "react";
 import { StudentContext } from "@/context/student-context";
@@ -56,15 +56,15 @@ function StudentHomePage() {
     <div className="min-h-screen bg-white">
       <section className="flex flex-col lg:flex-row items-center justify-between py-8 px-4 lg:px-8">
         <div className="lg:w-1/2 lg:pr-12">
-          <h1 className="text-4xl font-bold mb-4">Learning thet gets you</h1>
-          <p className="text-xl">
-            Skills for your present and your future. Get Started with US
+          <h1 className="text-4xl font-bold mb-4">Empowering Seniors with Digital Confidence</h1>
+          <p className="text-xl mb-4">
+          Discover a simple, intuitive app designed to help older adults master essential digital skills. From online safety to social media and video calls, our app makes learning easy, accessible, and rewarding.
           </p>
         </div>
         <div className="lg:w-full mb-8 lg:mb-0">
           <img
             src={banner}
-            width={600}
+            width={400}
             height={400}
             className="w-full h-auto rounded-lg shadow-lg"
           />
@@ -86,7 +86,7 @@ function StudentHomePage() {
         </div>
       </section>
       <section className="py-12 px-4 lg:px-8">
-        <h2 className="text-2xl font-bold mb-6">Featured COourses</h2>
+        <h2 className="text-2xl font-bold mb-6">Featured Courses</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {studentViewCoursesList && studentViewCoursesList.length > 0 ? (
             studentViewCoursesList.map((courseItem) => (
@@ -106,7 +106,7 @@ function StudentHomePage() {
                     {courseItem?.instructorName}
                   </p>
                   <p className="font-bold text-[16px]">
-                    ${courseItem?.pricing}
+                    NGN {courseItem?.pricing}
                   </p>
                 </div>
               </div>
