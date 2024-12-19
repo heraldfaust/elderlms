2bbda641c71f513b372b55411df358289ddd1ddc
Eaelllfaust <eaelllfaust@gmail.com>
2024-12-19 11:41:31 +0100

    changes

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "elderlms-rb6x.vercel.app",
+  baseURL: "https://elderlms-rb6x.vercel.app",
 });
 
 axiosInstance.interceptors.request.use(
